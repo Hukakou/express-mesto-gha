c5e3d59c6ba8e1353b63f77298035f1b13e13202
Vladimir <derekpro@mail.ru>
2023-06-04 15:54:07 +0300

    sprint 14 check token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,13 +6,13 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  let payload;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
-  let payload;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
